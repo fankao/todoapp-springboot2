bad57a54e067ee0a3ac1318e97cf1bbe02891d2d
chien <minhchiendc@gmail.com>
2020-04-13 08:52:55 +0700

    Domain Model: Todo

---
+++ src/main/java/com/apress/todo/domain/Todo.java
@@ -0,0 +1,29 @@
+package com.apress.todo.domain;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class Todo {
+    @NotNull
+    private String id;
+    @NotNull
+    @NotBlank
+    private String description;
+    private LocalDateTime created;
+    private LocalDateTime modified;
+    private  boolean completed;
+
+    public Todo() {
+        LocalDateTime date = LocalDateTime.now();
+        this.id = UUID.randomUUID().toString();
+        this.created = date;
+        this.modified = date;
+
+    }
+}
