2e2c88fc364fcc56dcd85a243a0c1437cbd10b53
chien <minhchiendc@gmail.com>
2020-04-13 09:31:46 +0700

    Validation: ToDoValidationErrorBuilder(Factory class that easily creates a ToDoValidationError instance with all the necessary information.)

---
+++ src/main/java/com/apress/todo/validation/TodoValidationErrorBuilder.java
@@ -0,0 +1,14 @@
+package com.apress.todo.validation;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+
+public class TodoValidationErrorBuilder {
+    public TodoValidationError fromBindingError(Errors errors) {
+        TodoValidationError error = new TodoValidationError("Validation Failed. " + errors.getErrorCount() + " error(s)");
+        for(ObjectError objectError:errors.getAllErrors()){
+            error.addValidationError(objectError.getDefaultMessage());
+        }
+        return error;
+    }
+}
